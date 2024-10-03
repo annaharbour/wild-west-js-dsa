4576470b595435a544d15d0c50b1225cb4b7a618
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2024-10-03 14:26:34 -0400

    rest of SLL methods and tests

--- linkedList/singlyLinkedList.js
+++ linkedList/singlyLinkedList.js
@@ -1,5 +1,6 @@
 class Node {
 	constructor(data = null) {
+		// initialize data to be either the value passed in to the new node during creation and the next node to null
 		this.data = data;
 		this.next = null;
 	}
@@ -7,6 +8,7 @@ class Node {
 
 class SinglyLinkedList {
 	constructor(val) {
+		// initialize head and tail to null and length to 0
 		this.head = null;
 		this.tail = null;
 		this.length = 0;
@@ -14,53 +16,152 @@ class SinglyLinkedList {
 
 	push(val) {
 		const node = new Node(val);
+		// Checks to see if there's already a head - if not, the list is empty, so it sets the head and tail as the new node.
 		if (!this.head) {
 			this.head = node;
 			this.tail = this.head;
 		} else {
+			// if not, we set the next node after the current tail to be the new node and set the new node to be the tail of the linked list.
 			this.tail.next = node;
 			this.tail = node;
 		}
+		// length property increases by one
 		this.length++;
+		// return the linked list
 		return this;
 	}
 
 	pop() {
+		// if there's no head to pop off, the list is empty - return undefined
 		if (!this.head) {
 			return undefined;
 		}
+		// otherwise set the current value to be the head
 		let curr = this.head;
+		// and the tail to be the current value
 		let tail = curr;
+		// while there's a next node
 		while (curr.next) {
+			// set tail to be the current value, and move our "current" pointer to the next node
 			tail = curr;
 			curr = curr.next;
 		}
+		// once there's not a next node, set the list's tail property to the tail
 		this.tail = tail;
+		// remove the pointer to the next node
 		this.tail.next = null;
+		// reduce the list's length property by 1
 		this.length -= 1;
+		// if this action empties the list, set the list's length property to 0 and the head and tail to be null.
 		if (this.length === 0) {
 			this.head = null;
 			this.tail = null;
 		}
+		// return the popped-off node, which was the tail
 		return curr;
 	}
 
 	get(idx) {
+		// set our temporary variable to the head of the list
 		let curr = this.head;
-		let count = 0
-		if(count === idx){
-			return this.head;
+		// set count to 0
+		let count = 0;
+		// check to make sure the list isn't empty
+		while (curr !== null) {
+			// once we reach the desired index, return the node's value
+			if (idx === count) {
+				return curr;
+			}
+			// else, increment count and move to the next node
+			count++;
+			curr = curr.next;
 		}
-		
+		// if current is null, return null
+		return null;
 	}
 
-	set() {}
+	set(idx, val) {
+		let node = this.get(idx);
+		if (node) {
+			node.data = val;
+			return true;
+		} else return false;
+	}
 
-	insert() {}
+	insert(idx, val) {
+		// Check to make sure the index parameter is in bounds of the list
+		if (idx < 0 || idx > this.length) return false;
+		// Create a new node with the value
+		let node = new Node(val);
+		// if the index is 0
+		if (idx === 0) {
+			// set the new node to be the head
+			node.next = this.head;
+			// set the head to be the new node
+			this.head = node;
+			// if the list is empty, the node is also the new tail
+			if (this.length === 0) this.tail = node;
+			// increment the length of the list by one
+			this.length++;
+			return true;
+		}
+		// if the index is the same as the length of the array
+		if (idx === this.length) {
+			// then we can just push the value onto the end of the list, and the node becomes the new tail
+			this.push(val);
+			return true;
+		}
+		// otherwise set a temporary variable to equal the current node at that index, set the next property of the previous node to the new node, and the new node's next property to the previous node's former next node
+		let prev = this.get(idx - 1);
+		let temp = prev.next;
+		prev.next = node;
+		node.next = temp;
+		this.length++;
+		return true;
+	}
 
-	remove() {}
+	remove(idx) {
+		if (!this.head || idx < 0 || idx >= this.length) return undefined;
+		if (idx === 0) {
+			let head = this.head;
+			this.head = head.next;
+			this.length--;
+			if (this.length === 0) {
+				this.tail = null;
+			}
+			return head;
+		}
+		let prev = this.get(idx - 1);
+		let node = prev.next;
+		prev.next = node.next;
+		this.length--;
+		return node;
+	}
 
-	rotate() {}
+	rotate(n) {
+		if (this.length === 0 || n <= 0) return;
+
+		n = n % this.length;
+		if (n === 0) return;
+
+		let current = this.head;
+		let count = 1;
+		while (count < this.length - n && current !== null) {
+			current = current.next;
+			count++;
+		}
+
+		let newHead = current.next;
+		current.next = null;
+		this.tail.next = this.head;
+		this.head = newHead;
+
+		let temp = newHead;
+		while (temp.next !== null) {
+			temp = temp.next;
+		}
+		this.tail = temp;
+	}
 }
 
 module.exports = { SinglyLinkedList, Node };
--- linkedList/test.js
+++ linkedList/test.js
@@ -14,14 +14,12 @@ describe("A Node", () => {
 });
 
 describe("A SinglyLinkedList", () => {
+	test("SinglyLinkedList is a class", () => {
+		expect(typeof SinglyLinkedList.prototype.constructor).toEqual("function");
+	});
 	let list;
-
 	beforeEach(() => {
 		list = new SinglyLinkedList();
-	});
-
-	test("SinglyLinkedList is a class", () => {
-		expect(typeof list.prototype.constructor).toEqual("function");
 	});
 	test("A SinglyLinkedList has properties of head, tail, and length", () => {
 		expect(list.head).toEqual(null);
@@ -31,6 +29,10 @@ describe("A SinglyLinkedList", () => {
 });
 
 describe("Push", () => {
+	let list;
+	beforeEach(() => {
+		list = new SinglyLinkedList();
+	});
 	test("Push is a function", () => {
 		expect(typeof list.push).toEqual("function");
 	});
@@ -46,6 +48,11 @@ describe("Push", () => {
 });
 
 describe("Pop", () => {
+	let list;
+
+	beforeEach(() => {
+		list = new SinglyLinkedList();
+	});
 	test("Pop is a function", () => {
 		expect(typeof list.pop).toEqual("function");
 	});
@@ -53,39 +60,106 @@ describe("Pop", () => {
 		list.push(10);
 		list.push(15);
 
-		expect(list.pop()).toEqual(15);
+		expect(list.pop().data).toEqual(15);
 		expect(list.tail.data).toEqual(10);
 		expect(list.length).toEqual(1);
 	});
 });
 
-describe.skip("Get", () => {
-	test.skip("Get is a function", () => {
-		expect(typeof get).toEqual("function");
-	});
-	test.skip("This function should find a node at a specified index in a SinglyLinkedList. It should return the found node.", () => {});
-});
+describe("Get", () => {
+	let list;
 
-describe.skip("Set", () => {
-	test.skip("Set is a function", () => {
-		expect(typeof set).toEqual("function");
+	beforeEach(() => {
+		list = new SinglyLinkedList();
 	});
-});
-
-describe.skip("Insert", () => {
-	test.skip("Insert is a function", () => {
-		expect(typeof insert).toEqual("function");
+	test("Get is a function", () => {
+		expect(typeof list.get).toEqual("function");
 	});
-});
+	test("This function should find a node at a specified index in a SinglyLinkedList. It should return the found node's value.", () => {
+		list.push(10);
+		list.push(15);
 
-describe.skip("Remove", () => {
-	test.skip("Remove is a function", () => {
-		expect(typeof remove).toEqual("function");
+		expect(list.get(0).data).toEqual(10);
+		expect(list.get(1).data).toEqual(15);
+		expect(list.get(2)).toEqual(null);
 	});
 });
 
-describe.skip("Rotate", () => {
-	test.skip("Rotate is a function", () => {
-		expect(typeof rotate).toEqual("function");
+describe("Set", () => {
+	let list;
+
+	beforeEach(() => {
+		list = new SinglyLinkedList();
+	});
+	test("Set is a function", () => {
+		expect(typeof list.set).toEqual("function");
+	});
+	test("Set should accept an index and a value and update the value of the node at that index in a singly linked list with the provided value. Should return true for successful update to a node and false for an invalid index", () => {
+		list.push(10);
+		list.push(15);
+
+		expect(list.set(0, 20)).toBe(true);
+		expect(list.set(3, 30)).toBe(false);
+		expect(list.get(0).data).toEqual(20);
+		expect(list.get(3)).toEqual(null);
+	});
+});
+
+describe("Insert", () => {
+	let list;
+
+	beforeEach(() => {
+		list = new SinglyLinkedList();
+	});
+
+	test("Insert is a function", () => {
+		expect(typeof list.insert).toEqual("function");
+	});
+
+	test("Insert should insert a node at a specified index and return true if the index is valid or false if the index is invalid ", () => {
+		list.push(5);
+		list.push(10);
+		expect(list.insert(0, 15)).toBe(true);
+		expect(list.insert(1, 20)).toBe(true);
+		expect(list.insert(100, 25)).toBe(false);
+	});
+});
+
+describe("Remove", () => {
+	let list;
+
+	beforeEach(() => {
+		list = new SinglyLinkedList();
+	});
+	test("Remove is a function", () => {
+		expect(typeof list.remove).toEqual("function");
+	});
+	test("Removes a node at a given index from the list and returns the removed node if the index is valid or undefined if the index is invalid", () => {
+		list.push(5);
+		list.push(10);
+		list.push(15);
+
+		expect(list.remove(0).data).toEqual(5);
+		expect(list.remove(1).data).toEqual(15);
+		expect(list.remove(2)).toBe(undefined);
+	});
+});
+
+describe("Rotate", () => {
+	let list;
+
+	beforeEach(() => {
+		list = new SinglyLinkedList();
+	});
+
+	test("Rotate is a function", () => {
+		expect(typeof list.rotate).toEqual("function");
+	});
+
+	test("Should rotate all the nodes in the list by some number passed in. For instance, 1 -> 2 -> 3 -> 4 -> 5 rotated by 2 becomes 3 -> 4 -> 5 -> 1 -> 2. ", () => {
+		list.push(5).push(10).push(15).push(20).push(25);
+		list.rotate(3);
+		expect(list.get(0).data).toEqual(15);
+		expect(list.get(1).data).toEqual(20);
 	});
 });
